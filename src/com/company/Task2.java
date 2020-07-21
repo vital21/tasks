3a3455376059b6a367b0feba49ea04472791ed67
vital21 <vitaliysaydak@gmail.com>
2020-07-21 18:39:01 +0300

    я изменил

--- src/com/company/Task2.java
+++ src/com/company/Task2.java
@@ -1,13 +1,12 @@
 package com.company;
 
+import java.time.LocalDate;
 import java.time.LocalTime;
 
 public class Task2 {
     public static void main(String[] args) {
-        //Фигня переделай
-        //Сделай что бы выводил текущую дату!
-        LocalTime time=LocalTime.now();
-        System.out.println(time);
+        LocalDate data=LocalDate.now();
+        System.out.println(data);
     }
 
 }
