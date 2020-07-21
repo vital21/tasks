edff4b45829b2d7fa7cb0950b861702b0a85e756
Alex Golubev <golubevaleksej7@gmail.com>
2020-07-21 17:34:32 +0200

    В Task2 переделай!

--- src/com/company/Task2.java
+++ src/com/company/Task2.java
@@ -4,6 +4,8 @@ import java.time.LocalTime;
 
 public class Task2 {
     public static void main(String[] args) {
+        //Фигня переделай
+        //Сделай что бы выводил текущую дату!
         LocalTime time=LocalTime.now();
         System.out.println(time);
     }
