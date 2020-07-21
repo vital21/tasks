b61e9a25944a5cbccf177269c150eabc77fe8957
Alex Golubev <golubevaleksej7@gmail.com>
2020-07-21 17:12:35 +0200

    chanche Task1

--- src/com/company/Tasks1.java
+++ src/com/company/Tasks1.java
@@ -1,10 +1,15 @@
 package com.company;
 
 public class Tasks1 {
+    //НЕ правильно, нужно написать без static
     private static String s="world";
-
+    public String toString(){
+        return s;
+    }
     public static void main(String[] args) {
-        System.out.println(s);
+        //Вот правильное решение!
+        Tasks1 tasks = new Tasks1();
+        System.out.println(tasks);
 
     }
 }
