1eed46d943b0cc4f16d3e127142232eb31883b50
vital21 <vitaliysaydak@gmail.com>
2020-07-21 23:36:54 +0300

    я сделал первое задание)) гыы

---
+++ src/com/company/MyFirstTask.java
@@ -0,0 +1,22 @@
+package com.company;
+import java.util.Scanner;
+public class MyFirstTask {
+
+
+        public static void main(String[] args) {
+            System.out.println("Plize enter namber for c and b and a");
+            Scanner num=new Scanner(System.in) ;
+            int z,a,b,c;
+
+            System.out.println("Enter a");
+            a=num.nextInt();
+            System.out.println("Enter b");
+            b=num.nextInt();
+            System.out.println("Enter c");
+            c=num.nextInt();
+            z=((a-3)*b/2)+c;
+            System.out.println("rezults is:"+z);
+        }
+
+    }
+
--- src/com/company/Task2.java
+++ src/com/company/Task2.java
@@ -2,11 +2,12 @@ package com.company;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.util.concurrent.TimeUnit;
 
 public class Task2 {
-    public static void main(String[] args) {
-        LocalDate data=LocalDate.now();
-        System.out.println(data);
+    public static void main(String[] args) throws InterruptedException {
+        long time=System.nanoTime();
+        System.out.println(System.nanoTime()-time);
     }
 
 }
