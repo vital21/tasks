52902f829f6b79dca19a0fe5acb3a3e4d75284b4
vital21 <vitaliysaydak@gmail.com>
2020-07-21 18:32:28 +0300

    я создал класс task2 который выводит текущее время

---
+++ src/com/company/Task2.java
@@ -0,0 +1,11 @@
+package com.company;
+
+import java.time.LocalTime;
+
+public class Task2 {
+    public static void main(String[] args) {
+        LocalTime time=LocalTime.now();
+        System.out.println(time);
+    }
+
+}
